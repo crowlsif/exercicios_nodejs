f12a9a6544652c75fc1bff01a5aac9897515f5d6
Eric Antunes <eric.antunes.silva@gmail.com>
2020-06-21 18:22:09 -0300

    atividade 1 ok

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -3,3 +3,14 @@
 
 
 // ## Faça o código ##
+let rs = require("readline-sync")
+
+let num = rs.question("Olá, favor informar o número do vôo. \n")
+
+let fileira = rs.question("Em qual das fileiras você deseja se acomodar? (A, B, C, D ou F) \n")
+
+console.log("======================================================================================================")
+
+console.log("Passageiro, o número do seu vôo é " + num + " acomode-se na fileira " + fileira + ". Desejamos uma boa viagem.")
+
+console.log("======================================================================================================")
