892e5518f15416aa6fbecc365276bab385e22bcd
Eric Antunes <eric.antunes.silva@gmail.com>
2020-06-22 21:50:42 -0300

    atividade 4 modificada para switch

--- atividade_4/index.js
+++ atividade_4/index.js
@@ -3,33 +3,73 @@
 
 
 // ## Faça o código ##
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
 let rs = require ("readline-sync")
 
-let month = rs.question("Em qual mês você nasceu? \n")
+let mes = rs.questionInt("Em qual mês você nasceu? (Dê 1 a 12) \n")
 
-let day = rs.questionInt("E qual o dia? \n")
+let dia = rs.questionInt("E qual o dia? \n")
+
+switch (mes) {
+    case 1:
+         dia >= 21 ? console.log("Aquário") : console.log("Capricórnio");
+        break;
+
+    case 2:
+        dia >= 19 ? console.log("Peixes") : console.log("Aquário");
+        break;
+    
+    case 3:
+         dia >= 21 ? console.log("Áries") : console.log("Peixes");
+        break;
+
+    case 4:
+        dia >= 21 ? console.log("Touro") : console.log("Áries");
+        break;
+
+    case 5:
+         dia >= 21 ? console.log("Gêmeos") : console.log("Touro");
+        break;
+
+    case 6:
+        dia >= 21 ? console.log("Câncer") : console.log("Gêmeos");
+        break;
+
+    case 7:
+         dia >= 23 ? console.log("Leão") : console.log("Câncer");
+        break;
+
+    case 8:
+        dia >= 23 ? console.log("Virgem") : console.log("Leão");
+        break;
+    
+    case 9:
+         dia >= 23 ? console.log("Libra") : console.log("Virgem");
+        break;
+
+    case 10:
+        dia >= 23 ? console.log("Escorpião") : console.log("Libra");
+        break;
+
+    case 11:
+         dia >= 22 ? console.log("Sagitário") : console.log("Escorpião");
+        break;
+
+    case 12:
+        dia >= 22 ? console.log("Capricórnio") : console.log("Sagitário");
+        break;
+
+    default:
+        console.log("Inválido")
+        break;
+}
 
 
-if (day >= 21 && month == "março" || day <=20 && month == "abril"){
-    console.log("Você é do signo de é Áries")
-} else if (day >= 21 && month == "abril" || day <=20 && month == "maio"){
-    console.log("Você é do signo de Touro")
-} else if (day >= 21 && month == "maio" || day <=20 && month == "junho"){
-        console.log("Você é do signo de Gêmeos")
-} else if (day >= 21 && month == "junho" || day <=22 && month == "julho"){
-    console.log("Você é do signo de Câncer")
-} else if (day >= 23 && month == "julho" || day <=22 && month == "agosto"){
-    console.log("Você é do signo de Leão")
-} else if (day >= 23 && month == "agosto" || day <=22 && month == "setembro"){
-    console.log("Você é do signo de Virgem")
-} else if (day >= 23 && month == "setembro" || day <=22 && month == "outubro"){
-    console.log("Você é do signo de Libra")
-} else if (day >= 23 && month == "outubro" || day <=21 && month == "novembro"){
-    console.log("Você é do signo de Escorpião")
-} else if (day >= 22 && month == "novembro" || day <=21 && month == "dezembro"){
-    console.log("Você é do signo de Sagitário")
-} else if (day >= 22 && month == "dezembro" || day <=20 && month == "janeiro"){
-    console.log("Você é do signo de Capricórnio")
-} else if (day >= 21 && month == "janeiro" || day <=18 && month == "fevereiro"){
-    console.log("Você é do signo de Aquário")
-} else console.log("Você é do signo de Peixes")
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
